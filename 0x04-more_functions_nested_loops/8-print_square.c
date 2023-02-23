9c50a6796f9b24148dfe24a9baab1bd82b1e245e
sherryahmos473 <sherry.ahmos@gmail.com>
2023-02-23 06:37:40 -0800

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,10 +12,13 @@ void print_square(int size)
 {
 	int row, column;
 
+	if (size > 0)
+	{
 	for (row = 1; row <= size; ++row)
 	{
 		for (column = 1; column <= size; ++column)
 			_putchar('#');
 		_putchar('\n');
+	}
 	}
 }
